be33f39e705d2ff735427607552009f2ee7929d3
Shuxin Weng <shuxinweng@gmail.com>
2023-07-26 22:25:53 -0400

    clickable png

--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -16,10 +16,8 @@ const projectVariant = {
 };
 
 const Project = ({ title, description, link }) => {
-  const overlayStyles = `
-    absolute top-0 left-0 w-full h-full opacity-0 hover:opacity-90 transition duration-500
-    bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue
-  `;
+  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
+    bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
   const projectTitle = title.split(" ").join("-").toLowerCase();
 
   return (
